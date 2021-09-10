6d54a326b8a1314409d6f64d01cf687c2fa061d1
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-09-10 11:21:41 +0530

    Update chemapp.py

--- chemapp.py
+++ chemapp.py
@@ -1,31 +1,67 @@
 import streamlit as st
 import py3Dmol
+
 from streamlit.proto.Markdown_pb2 import Markdown
 
 def chemapp():
 
-    st.title("PDB-view | ineelhere")
-    col1, col2 = st.columns(2)
-    pdbid = col1.text_input("Please provide the Protein Data Bank (PDB) ID here")
-    presentation = col2.selectbox("Please select presentation style", ['sphere', 'stick', 'line', 'cross', 'cartoon'])
+    st.title("Chem-ineel-view | ineelhere")
+    
+    options = ["PubChem Compound CID",
+        "RCSB Protein Data Bank (PDB) ID"]
+    
+    selection = st.radio("What type of chemical identifier would you like to provide?", options)
+    
+    if selection==options[0]:
+        col1, col2 = st.columns(2)
 
-    if pdbid:
-        lm = py3Dmol.view(query='pdb:'+pdbid,width=680, height=500)
-        if presentation:
-            lm.setStyle({presentation:{'color':'spectrum'}})
+        pc_cid = col1.text_input("Please provide the PubChem Compound CID here")
+        presentation = col2.selectbox("Please select presentation style", ['stick', 'line', 'cross', 'cartoon', 'sphere' ])
+
+        if pc_cid:
+            lm = py3Dmol.view(query='cid:'+pc_cid,width=680, height=500)
+            if presentation:
+                lm.setStyle({presentation:{'color':'spectrum'}})
+             
+                
+            else:
+                st.warning("Select the presentation style from above")
+            lm.render()
+            t =lm.js()
+
+            f = open('viz.html', 'w')
+            f.write(t.startjs)
+            f.write(t.endjs)
+            f.close()
+
+            st.components.v1.html(open('viz.html', 'r').read(), width=800, height=500)
         else:
-            st.warning("Select the presentation style from above")
-        lm.render()
-        t =lm.js()
+            st.warning("Enter the PubChem Compound CID above.  \nClick [here](https://pubchem.ncbi.nlm.nih.gov/) to visit the PubChem website")       
 
-        f = open('viz.html', 'w')
-        f.write(t.startjs)
-        f.write(t.endjs)
-        f.close()
 
-        st.components.v1.html(open('viz.html', 'r').read(), width=800, height=500)
-    else:
-        st.warning("Enter the Protein Data Bank (PDB) ID above.  \nClick [here](https://www.rcsb.org/) to visit the PDB")
+    if selection==options[1]:
+        col1, col2 = st.columns(2)
+
+        pdbid = col1.text_input("Please provide the Protein Data Bank (PDB) ID here")
+        presentation = col2.selectbox("Please select presentation style", ['stick', 'line', 'cross', 'cartoon', 'sphere'])
+
+        if pdbid:
+            lm = py3Dmol.view(query='pdb:'+pdbid,width=680, height=500)
+            if presentation:
+                lm.setStyle({presentation:{'color':'spectrum'}})
+            else:
+                st.warning("Select the presentation style from above")
+            lm.render()
+            t =lm.js()
+
+            f = open('viz.html', 'w')
+            f.write(t.startjs)
+            f.write(t.endjs)
+            f.close()
+
+            st.components.v1.html(open('viz.html', 'r').read(), width=800, height=500)
+        else:
+            st.warning("Enter the Protein Data Bank (PDB) ID above.  \nClick [here](https://www.rcsb.org/) to visit the PDB website")
 
     st.markdown("""
     ___
