f6c70939ee564b8915ad36c38659c000dac30731
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-11-06 13:49:07 +0530

    Update ineelhere.py

--- ineelhere.py
+++ ineelhere.py
@@ -19,7 +19,7 @@ selections = ["About me 🙂",
             "Find frequencies of all words present in a sentence" ,
             "Visualize chemical structures", 
             "Realtime stats on the COVID19 situation in India",
-            "Jokes"]
+            "Random Programming Jokes"]
 
 response = st.sidebar.radio('', selections)
 
@@ -48,5 +48,3 @@ ___
 *More apps on the way!*
 </p> <h6 class="jumbotron-heading" align = 'center'><br> <a href="https://www.linkedin.com/in/indraneelchakraborty/" target="_blank"><img src="https://static-exp1.licdn.com/sc/h/al2o9zrvru7aqj8e1x2rzsrca" alt="Linkedin" width="32" height="32" > </a> <a href="https://sites.google.com/view/indraneelchakraborty" target="_blank"><img width="32" height="32" src="https://lh3.googleusercontent.com/mjVS_Izc6fGAvuaT0v--gb2so5mZvAbI5EUMUB41cWB7tpy81trBCR8rIlj8NoKgPzDWGN-Hs97NlW0T9W57YJ5z9A8QQWwXUYa_Zg=h120" alt="Google Sites" > </a> <a href="https://twitter.com/ineelhere" target="_blank"> <img src="https://abs.twimg.com/favicons/twitter.ico" alt="Twitter" width="32" height="32"> </a> <a href="https://www.youtube.com/channel/UCbIMzl7rOj0FkamVf_aBM8w" target="_blank"> <img src="https://www.youtube.com/s/desktop/28b67e7f/img/favicon_48.png" alt="YouTube" width="32" height="32"> </a> <a href="https://github.com/ineelhere" target="_blank"><img width="32" height="32" src="https://github.com/fluidicon.png" alt="Github" > </a> </h6><br> <p align=center > <a href="https://docs.google.com/forms/d/e/1FAIpQLSeZuuBTcglrHmKFfTwZ66HdHVYKge6kJ3cAtSCdF7e_8NMypg/viewform" class="btn btn-outline-success" target="_blank"><strong>Contact Me</strong></a> </p> </div> </section> </main>
 """, unsafe_allow_html=True)
-
-
